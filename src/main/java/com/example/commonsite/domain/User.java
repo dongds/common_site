ac0cfdf21f619a485dee1c98f117a55d5496c30f
dongd <ddaa1155k@gmail.com>
2025-01-07 18:46:08 +0900

    feat(dv): vds

---
+++ src/main/java/com/example/commonsite/domain/User.java
@@ -0,0 +1,9 @@
+package com.example.commonsite.domain;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Long id;
+    private String name;
+}
---
+++ src/test/java/com/example/commonsite/CommonSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commonsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
